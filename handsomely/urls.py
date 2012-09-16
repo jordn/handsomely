61c5285d08a26571459c548dd8f3f85f9aaa0419
mansour <mansour@handsome.ly>
2012-09-16 01:27:27 +0000

    fixing bugs

--- handsomely/urls.py
+++ handsomely/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from handsomely.views import index, about, register, for_salons, login, logout, profile
+from handsomely.views import index, about, register, for_salons, user_login, user_logout, profile
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
