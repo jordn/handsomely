d2272c863c9092e8ee883e72f414fe9f419711a1
mansour <mansour@handsome.ly>
2012-09-16 23:48:10 +0000

    about page

--- handsomely/views.py
+++ handsomely/views.py
@@ -156,10 +156,10 @@ def emailtest(request):
 	admin_mail = 'team@handsome.ly'
 	email = 'mansour@handsome.ly'
 	salonName = 'Jim SoleTraders'
-	message = '<html><head>Hi! </head><body>' + salonName
+	message = '<html><head>Hi! </head><body>\n' + salonName
 	message += ' is now free, why not head down now to avoid a queue?\n'
 	message += 'Your response: \n   <a href=\"http://www.handsome.ly/response?ans=YES\">YES</a> \n   <a href=\"http://www.handsome.ly/response?ans=NO\">NO</a> \n   <a href=\"http://www.handsome.ly/response?ans=CANCEL\">CANCEL</a> <br/>'
-	message += '\nThanks, the Handsome.ly team</body></html>'
+	message += '\nThanks, \nthe Handsome.ly team</body></html>'
 	send_mail('Handsomely Notification', message, admin_mail, [email], fail_silently=False)
 	return render_to_response('index.html', {})
 
