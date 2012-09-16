c6cd05eb910c290d113911ed8151bfe0d486efd5
mansour <mansour@handsome.ly>
2012-09-16 19:00:46 +0000

    fixing static thing

--- static/js/getsalons.js
+++ static/js/getsalons.js
@@ -1,10 +1,10 @@
 function getSalons(city, isLoggedIn) {
 	  //this is ajax
-	  $.get("../get_salons?city="+city, function(data){
+	  jQuery.get("../get_salons?city="+city, function(data){
 		//give these arguments, return Jason objcect
 		var dat = JSON.parse(data);
 		if (dat.length > 0) {
-			document.getElementById("errors").innerHTML="";
+			document.getElementById("salon_list").innerHTML="";
 			var result_output = "<b>Salons:</b>";
 			var salon_list = "<br><ul>";
 			for (var i = 0; i < dat.length; i++) {
@@ -19,7 +19,7 @@ function getSalons(city, isLoggedIn) {
 			 document.getElementById("salon_list").innerHTML=result_output + salon_list;
 		} else {
 			document.getElementById("salon_list").innerHTML="";
-			document.getElementById("errors").innerHTML="<b>No salons found</b>";
+			document.getElementById("salon_list").innerHTML="<b>No salons found</b>";
 		}
 	   }); 
 }
