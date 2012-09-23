e603abb5dba75b106a8e8cfa21651139c0088b5b
mansour <mansour@handsome.ly>
2012-09-23 21:50:59 +0000

    backend user stuff part 2

--- handsomely/views.py
+++ handsomely/views.py
@@ -114,6 +114,7 @@ def create_user(request):
     newHandsomelyUser.save()
     return render_to_response("thank_you.html", {"name" : name}, context_instance=RequestContext(request))
 
+# not sure what this view does...?
 def notify_users(request):
 	djangoUserID = request.user.id
 	return render_to_response('notify_users.html', {'djangoUserID' : djangoUserID}, context_instance=RequestContext(request))
