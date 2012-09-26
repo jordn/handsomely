32c5898d3cd661604a5c9158d4a191c1e23b8250
mansour <mansour@handsome.ly>
2012-09-26 19:18:54 +0200

    admin stuff

--- handsomely/admin.py
+++ handsomely/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from handsomely.models import Salon
+from handsomely.models import *
 
 admin.site.register(Salon)
+admin.site.register(SalonDetails)
+admin.site.register(PriceMenu)
+admin.site.register(SalonOpeningHours)
+admin.site.register(HandsomelyUser)
+admin.site.register(Customer)
+admin.site.register(Request)
+admin.site.register(Notification)
+
