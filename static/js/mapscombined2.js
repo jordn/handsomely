4b3fd55eaa708b83c30b972cf2ac20ca49d53078
Fraser Atkins <fraser@handsome.ly>
2012-10-02 10:57:47 +0100

    requests from Mansour

--- static/js/mapscombined2.js
+++ static/js/mapscombined2.js
@@ -117,9 +117,11 @@
 			function tellUsers(salonID){
 				if (LoggedInStatus == true){
 					if (djangoUserID != -1) {
+                        document.getElementById('getNotifiedButton').innerHTML = "Loading..."; 
 						jQuery.get("/create_notification_request/?salonID=" + salonID + "&djangoUserID=" + djangoUserID, function(data){ alert("We will let you know if times are free!");
 						});
-					}	
+                        document.getElementById('getNotifiedButton').innerHTML = "Done!"; 
+					}		
   				}
   				else{
   					login("show");
@@ -193,7 +195,8 @@
 									marker_content += "<br>" + "Sunday: " + sund[index][0] + "-" + sund[index][1];
 								}
 
-								marker_content += "<br><input type = \"button\" onClick=\"tellUsers(" + salonID + ")\" value = \"Email me when they are free!\">";
+								//marker_content += "<br><input type = \"button\" onClick=\"tellUsers(" + salonID + ")\" value = \"Email me when they are free!\">";
+								marker_content += "<br><input type = \"button\" onClick=\"tellUsers(" + salonID + ")\" id=\"getNotifiedButton\" value = \"Email me when they are free!\">"; 
 								var myboxOptions = {
 		                 			content: marker_content 
 					                ,disableAutoPan: false
