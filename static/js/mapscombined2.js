91e46fbd8e2e7ddf2be2c36edd7ff234fe2d0442
mansour <mansour@handsome.ly>
2012-10-02 23:18:45 +0100

    recentre map

--- static/js/mapscombined2.js
+++ static/js/mapscombined2.js
@@ -139,7 +139,7 @@
 				var geocoder = new google.maps.Geocoder();  
 			  	geocoder.geocode( { 'address': address}, function(results, status) {
 			   		if (status == google.maps.GeocoderStatus.OK) {
-			      		map.setCenter(new google.maps.LatLng(52.205029,0.121279));
+			      		map.setCenter(new google.maps.LatLng(52.205277,0.121945));
 			      		var marker = new google.maps.Marker({
 			          		map: map,
 			          		position: results[0].geometry.location
