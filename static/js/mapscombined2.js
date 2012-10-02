3b71386538f766a58a5207e6952be05c1ebd051d
Fraser Atkins <fraser@handsome.ly>
2012-10-02 21:14:44 +0100

    latest update of js

--- static/js/mapscombined2.js
+++ static/js/mapscombined2.js
@@ -81,7 +81,6 @@
 				//give these arguments, return Jason object
 				var dat = JSON.parse(data);
 				if (dat.length > 0) {
-					//document.getElementById("salon_list").innerHTML="";
 					var result_output = "<b>Salons:</b>";
 					var salon_list = "<br><ul>";
 					var salon_prices = "<br><ul>";
@@ -91,7 +90,7 @@
 						names[i] = (dat[i].fields.salonName);
 						phones[i] = (dat[i].fields.phone);
 						addresses[i] = (dat[i].fields.address);
-						parsePrice(dat[i].pk, i)		//taking the primary key, but this is not compatible with the salonID, cannot call with first argument PK as PKs do not reset
+						parsePrice(dat[i].pk, i)		
 						parseHours(dat[i].pk, i)
 					}
 				} 
@@ -150,7 +149,7 @@
 						var infowindow = new InfoBox();
 						google.maps.event.addListener(marker, 'click', (function(marker, index) {
 		        			return function() {
-								var marker_content = names[index];
+								var marker_content = "<b>" + names[index] + "</b>";
 								marker_content += "<br>" + cut[index] + ": &pound" + prices[index]; 
 								marker_content += "<br>" + phones[index];
 
@@ -203,7 +202,6 @@
 									marker_content += "<br>" + "Sunday: " + sund[index][0] + "-" + sund[index][1];
 								}
 
-								//marker_content += "<br><input type = \"button\" onClick=\"tellUsers(" + salonID + ")\" value = \"Email me when they are free!\">";
 								marker_content += "<br><input type = \"button\" onClick=\"tellUsers(" + salonID + ")\" id=\"getNotifiedButton\" value = \"Email me when they are free!\">"; 
 								var myboxOptions = {
 		                 			content: marker_content 
@@ -213,7 +211,6 @@
 					                ,zIndex: null
 									,shadowStyle: 1
 					                ,boxStyle: { 
-					                  	//background: "url('tipbox.gif') no-repeat"
 					                  	border: "1px solid black"
 
 										,backgroundColor: 'rgb(255,255,255)'
