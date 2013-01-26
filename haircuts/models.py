caead7491f3d7692e58e291eb3a9340bc023ac61
mansour <mansour@handsome.ly>
2013-01-26 02:04:35 +0000

    edited models

--- haircuts/models.py
+++ haircuts/models.py
@@ -1,16 +1,20 @@
 from django.db import models
+from django_localflavor_gb.forms import GBPostcodeField
 
-class HandsomelyUser(User): # Extends User model
-	django_user_id = models.OneToOneField(User) # Inherit from User model
+class HandsomelyUser(User): 
+	django_user_id = models.OneToOneField(User) 
 	gender_choices = ( ('M', 'Male'), ('F','Female') )
 	gender = models.CharField(max_length=1, choices=gender_choices)
-	is_salon = models.BooleanField()
+	is_salon = models.BooleanField(default=False)
 	confirmed = Models.BooleanField(default=False)
 	
 class Salon(models.Model):
 	handsomely_user_id = models.ForeignKey('HandsomelyUser')
 	salon_name = models.CharField(max_length=50)
-	address = models.CharField(max_length=100)
+	address_line_1 = models.CharField(max_length=100)
+	address_line_2 = models.CharField(max_length=100)
+	city = models.CharField(max_length=100)
+	postcode = GBPostcodeField()
 	phone_number = models.CharField(max_length=20)
 	website = models.URLField()
 	mens_standard_price = models.DecimalField(max_digits=4, decimal_places=2)
@@ -25,12 +29,13 @@ class Request(models.Model):
 	haircut_type = (max_length=1, choices=haircut_choices) 
 	status_choices =  ( ('FULF', 'Fulfilled'), ('CANC', 'Cancelled'), ('WAIT', 'Waiting') )
 	status = (max_length=4, choices=status_choices) 
-	start_date_time = models.DateTimeField()
+	start_date_time = models.DateTimeField(default=datetime.now) 
+	#notification_id
 	
 class Notification(models.Model):
-	issue_date_time = models.DateTimeField()
+	issue_date_time = models.DateTimeField(default=datetime.now)
 	salon_id = models.ForeignKey('Salon')
-	request_ids = models.ManyToManyField()
+	#request_ids = models.ManyToManyField() #TODO
 	filled_by = models.ForeignKey('HandsomelyUser')
 	status_choices = ( ('FULF', 'Fulfilled'), ('CANC', 'Cancelled'), ('WAIT', 'Waiting') )
 	status = (max_length=4, choices=haircut_choices) 
