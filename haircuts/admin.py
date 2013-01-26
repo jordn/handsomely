8e9fff8c5827dd71f6cc9ab3be90cec7ed79f81d
Fraser Atkins <fraser@handsome.ly>
2013-01-26 13:16:33 +0000

    Register models for admin

--- haircuts/admin.py
+++ haircuts/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin
-from haircuts.models import *
+from haircuts.models import HandsomelyUser, Salon, Request, Notification
+
+admin.site.register(HandsomelyUser)
+admin.site.register(Salon)
+admin.site.register(Request)
+admin.site.register(Notification)
